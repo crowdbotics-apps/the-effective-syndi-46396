806422b715c382119f4bb8992089e861b5d51c39
Crowdbotics <team@crowdbotics.com>
2024-02-16 14:58:55 +0000

    Initial commit

---
+++ backend/the_effective_syndi_46396/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for the_effective_syndi_46396 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "the_effective_syndi_46396.settings"
+)
+
+application = get_wsgi_application()
